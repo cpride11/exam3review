ada01e76ac151adf221abecbbc6af50cfcf44dde
cj89112 <cpride@una.edu>
2024-04-01 23:06:36 -0500

    updates

--- api.js
+++ api.js
@@ -76,24 +76,3 @@ myapp.delete('/characters/:id', (req, res) => {
 });
 
 myapp.listen(8080);
-
-
-
-
-/*myapp.get(['/', '/index.html'], (req, res) => {
-    console.log('req for root, sending file ${__dirname}/public/index.html');
-    res.sendFile('${__dirname}/public/index.html'); 
-});
-
-myapp.get('/student/:id', (req, res) => {
-    res.status(200).send({"id": "665343"});
-    res.end();
-});
-
-myapp.post('/student/:id', (req, res) => {
-    const givenID = req.params.id;
-    res.status(200).send({"id": givenID});
-    res.end();
-});
-
-myapp.listen(8080);*/
